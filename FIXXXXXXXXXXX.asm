fae2f47df3cfeccac59961b03bb0320086c4aa00
hadi <40880027+idahdam@users.noreply.github.com>
2021-04-17 20:31:03 +0700

    Add files via upload

--- FIXXXXXXXXXXX.asm
+++ FIXXXXXXXXXXX.asm
@@ -1,11 +1,11 @@
-; ================================================================;
+	; ================================================================;
 ; Kelompok B5 - Praktikum Siber Fisik		 		  ;
 ; 
 ; Whack A Mole					 		  ;
 ; 
 ; Anggota:
 ; 1. Muhammad Hadi - 1906355623
-; 2. Ahmad Fakhri Mirfananda - 1906300656
+; 2. Ahmad Fakhri Mirfananda - 190630065
 ; 3. Ananda Luqman Mahendra - 190630067
 ; 4. Seno Aji Wicaksono - 1906300851
 ;                                         ;
@@ -16,23 +16,27 @@
 ; ================================================================;
 
 ; declaring delay per difficulties
-DELAY_EASY equ 10h ; membuat variabel untuk delay easy
-DELAY_MED equ 6h ; membuat variabel untuk delay medium
-DELAY_HARD equ 3h ; membuat variabel untuk delay hard
-PATTERN equ 6h ;banyaknya pattern yang akan muncul yaitu 6x
-SHIFT_AMOUNT equ 9h ;banyaknya shift yang akan dilakukan pada register A
+DELAY_EASY equ 10h
+DELAY_MED equ 6h
+DELAY_HARD equ 3h ;R0
+PATTERN equ 6h ;R1
+SHIFT_AMOUNT equ 9h ;R2
 rand16reg equ 021h ;two bytes
 
 ; main program calls
 ORG 0h
-	JMP START_GAME ;Jump ke Start game
+	JMP START_GAME
 	
-COUNTUP: ;Count up untuk menambah skor
+COUNTUP:
 	MOV P2, A
+	CJNE A, #0ffh, LETSGO
+	CLR A
+	RET
+LETSGO:
 	DJNZ R2, START_SHIFT
 	RET
 	
-START_SHIFT: ;Shifting pada register A 
+START_SHIFT:
 	JNB P2.0, SHIFTING
 	INC R3
 	MOV A, R3
@@ -41,32 +45,32 @@ START_SHIFT: ;Shifting pada register A
 	CLR A
 	JMP SHIFTING
 	
-SHIFTING: ;Proses Shifting
+SHIFTING:
 	RR A
 	JMP COUNTUP
 	
-START_GAME: ;Memulai game
-	MOV DPTR, #LUT ;Mengisi DPTR dengan alamat Look up table
-	;MOV A,#11111111B ; Memasukkan A dengan 1 semua
-	MOV P0,#00000000B ; P0 sebagai output
-	MOV R3, #0FFh ;Mengisi R3 untuk skor dengan FF agar jika diincrement akan dimulai dari 0
+START_GAME:
+	MOV DPTR, #LUT ;moves starting address of LUT to DPTR
+	;MOV A,#11111111B ; loads A with all 1's
+	MOV P0,#00000000B ; initializes P0 as output port
+	MOV R3, #0FFh
 	MOV R5, #0h
-	JNB P2.7, EASY ;Jump jika difficulty = easy 
-	JNB P2.3, MEDIUM ;Jump jika difficulty = medium 
-	JNB P2.2, HARD ;Jump jika difficulty = hard 
-	JMP START_GAME ;Looping kembali ke awal game jika satu round sudah selesai
+	JNB P2.7, EASY
+	JNB P2.3, MEDIUM
+	JNB P2.2, HARD
+	JMP START_GAME
 
 EASY:
 	SETB P2.7
-	CALL SET_DIFFICULTY_E ;Memanggil fungsi untuk set difficulty delay easy
+	CALL SET_DIFFICULTY_E
 	JMP MAIN_GAME
 MEDIUM:
 	SETB P2.3
-	CALL SET_DIFFICULTY_M ;Memanggil fungsi untuk set difficulty delay medium
+	CALL SET_DIFFICULTY_M
 	JMP MAIN_GAME
 HARD:
 	SETB P2.2
-	CALL SET_DIFFICULTY_H ;Memanggil fungsi untuk set difficulty delay hard
+	CALL SET_DIFFICULTY_H
 	JMP MAIN_GAME
 	
 SET_DIFFICULTY_E:
@@ -76,19 +80,17 @@ SET_DIFFICULTY_E:
 	RET
 	
 SET_DIFFICULTY_M:
-	; set delay di sini untuk medium
 	MOV R0, #DELAY_MED
 	MOV R1, #PATTERN
 	RET
 	
 SET_DIFFICULTY_H:
-	; set delay di sini untuk hard
 	MOV R0, #DELAY_HARD
 	MOV R1, #PATTERN
 	RET
 
 DELAY:
-	;Fungsi delay berdasarkan timer yang dipengaruhi difficulty
+	
 	MOV TMOD, #00000001B
 	MOV TL1, #00H 
 	MOV TH1, #00H
@@ -98,12 +100,12 @@ AGAIN:
 	JNB TF1, AGAIN
 	CLR TR1
 	CLR TF1
-	DJNZ R0, DELAY ;looping untuk banyaknya delay berdasarkan difficulty
+	DJNZ R0, DELAY
 	RET
 
 
 MAIN_GAME:
-	;Fungsi main game mulai disini
+	 
 	MOV R2, #SHIFT_AMOUNT
 	CALL RAND16
 	MOV A, B
@@ -125,19 +127,19 @@ MAIN_GAME:
 	JMP START_GAME
 
 
-EXIT_GAME: ;Fungsi untuk keluar dari game
+EXIT_GAME:
 	JMP END_GAME
 
-; implementasi keypad
+; keypad implementation
 BACK:
-	MOV P1,#11111111B ;Memasukkan P1 dengan 1 semua
-     	CLR P1.0  ;membuat p1.0 atau baris pertama menjadi low
-     	JB P1.4,NEXT1  ; memeriksa jika kolom 1 low dan jump ke NEXT1 jika tidak low
-     	MOV A,#0D   ; Memasukkan A dengan nilai 0 jika kolom low yaitu saat switch ditekan
-     	ACALL DISPLAY  ; Memanggil subroutine untuk display
+	MOV P1,#11111111B ;loads P1 with all 1's
+     	CLR P1.0  ;makes row 1 low
+     	JB P1.4,NEXT1  ; checks whether column 1 is low and jumps to NEXT1 if not low
+     	MOV A,#0D   ; loads a with 0D if column is low (that means key 1 is pressed)
+     	ACALL DISPLAY  ; calls DISPLAY subroutine
      	
 NEXT1:
-	JB P1.5,NEXT2 ; Prosedur yang sama dengan pada BACK dilakukan pada semua subroutine next lainnya
+	JB P1.5,NEXT2 ; checks whether column 2 is low and so on...
 	MOV A,#1D
 	ACALL DISPLAY
 NEXT2:
@@ -195,16 +197,16 @@ NEXT11:
        	MOV A,#14D
        	ACALL DISPLAY
        	
-CHECK_DONE: ;Pemeriksaan jika selesai
+CHECK_DONE:
 	RET
 
-DISPLAY:MOVC A,@A+DPTR ;Pengambilan data dari LUT berdasarkan skor
+DISPLAY:MOVC A,@A+DPTR ; gets digit drive pattern for the current key from LUT
 	CPL A
         ;MOV P0,A
-        CPL A      
+        CPL A      ; puts corresponding digit drive pattern into P0
         RET
 
-; randomizer untuk pattern
+; randomizer
 
 rand16:	
 	;ADDC A, #01010101b
@@ -231,8 +233,8 @@ rand16c:rlc	a
 	ret
 	
 ORG 200h
-LUT: ;Lookuptable untuk pemeriksaan pattern dengan input
-	DB 10000000B 
+LUT: 
+	DB 10000000B ; Look up table starts here
 	DB 01000000B 
 	DB 00100000B
 	DB 00000000B
